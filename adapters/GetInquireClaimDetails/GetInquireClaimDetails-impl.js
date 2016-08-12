4a2c8a264b85aa4016901208bd9fc0e6bf727563
InsuranceApp <madhurya.malladi@gmail.com>
2016-08-12 22:18:44 +0530

    Adding GetInquireClaimDetails-impl.js
    Adding GetInquireClaimDetails-impl.js

---
+++ adapters/GetInquireClaimDetails/GetInquireClaimDetails-impl.js
@@ -0,0 +1,90 @@
+/*
+ *  Licensed Materials - Property of IBM
+ *  5725-I43 (C) Copyright IBM Corp. 2011, 2013. All Rights Reserved.
+ *  US Government Users Restricted Rights - Use, duplication or
+ *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
+/**
+ *  WL.Server.invokeHttp(parameters) accepts the following json object as an argument:
+ *  
+ *  {
+ *  	// Mandatory 
+ *  	method : 'get' , 'post', 'delete' , 'put' or 'head' 
+ *  	path: value,
+ *  	
+ *  	// Optional 
+ *  	returnedContentType: any known mime-type or one of "json", "css", "csv", "plain", "xml", "html"  
+ *  	returnedContentEncoding : 'encoding', 
+ *  	parameters: {name1: value1, ... }, 
+ *  	headers: {name1: value1, ... }, 
+ *  	cookies: {name1: value1, ... }, 
+ *  	body: { 
+ *  		contentType: 'text/xml; charset=utf-8' or similar value, 
+ *  		content: stringValue 
+ *  	}, 
+ *  	transformation: { 
+ *  		type: 'default', or 'xslFile', 
+ *  		xslFile: fileName 
+ *  	} 
+ *  } 
+ */
+
+/**
+ * 
+ * @returns json list of items
+ */
+function getInquireClaimHTTPAdapters() {
+		var input = {
+	    method : 'get',
+	    returnedContentType : 'application/json',
+	    path : 'api/searchClaim/madhurya.malladi@gmail.com',
+	   
+	};
+	var result = WL.Server.invokeHttp(input);
+	var res=JSON.stringify(result);
+	return result;
+}
+
+
+
+function addGetInquireClaimDetail(param1) {
+	
+	var input = {
+	    method : 'put',
+	    returnedContentType : 'json',
+	    path : 'userInputRequired'
+	};
+	
+	
+	return WL.Server.invokeHttp(input);
+}
+
+
+function updateGetInquireClaimDetail(param1) {
+	
+	var input = {
+	    method : 'post',
+	    returnedContentType : 'json',
+	    path : 'userInputRequired'
+	};
+	
+	
+	return WL.Server.invokeHttp(input);
+}
+
+
+function deleteGetInquireClaimDetail(param1) {
+	
+	
+	var input = {
+	    method : 'delete',
+	    returnedContentType : 'json',
+	    path : 'userInputRequired'
+	};
+	
+	
+	return WL.Server.invokeHttp(input);
+}
+
+
